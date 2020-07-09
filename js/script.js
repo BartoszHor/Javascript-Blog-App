5160cebaf461321a419fb88fcae245e1cf486b36
Bartosz Horoba <bartosz.horoba@gmail.com>
2020-07-09 06:47:13 +0200

    Add class avtive to clicked element on left sidebar

--- js/script.js
+++ js/script.js
@@ -5,14 +5,26 @@
 */
 const titleClickHandler = function(event){
   console.log('Link was clicked!');
-  console.log(event)
+  console.log(event);
+  const clickedElement = this;
 
   /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll(".titles a.active")
+
+  for(let activeLink of activeLinks) {
+  	activeLink.classList.remove("active");
+  }
 
   /* add class 'active' to the clicked link */
+console.log("Clicked element: ", clickedElement)
+this.classList.add("active")
 
   /* remove class 'active' from all articles */
+const activeArticles = document.querySelectorAll(".posts .post.active")
 
+for(let activeArticle of activeArticles) {
+	activeArticle.classList.remove("active")
+}
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
